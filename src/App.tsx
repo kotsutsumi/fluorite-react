cddd1cd5da285a5e4f65fdde6aa463bbb7b7510e
Kazuhiro Kotsutsumi <kotsutsumi@gmail.com>
2023-12-07 04:12:37 +0900

    Install ReactRouter

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     return (
         <>
             <div>
-                <h1 className="text-3xl font-bold underline">Fluorite React</h1>
+                <h1 className="text-4xl font-bold underline">Fluorite React</h1>
                 <Button label="Click" />
             </div>
         </>
